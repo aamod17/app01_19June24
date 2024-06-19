46070ede3b90eafb76d0c10135750bdd66fce7fe
amod <aamodpoddar@gmail.com>
2024-06-19 23:39:48 +0530

    my second commit

--- src/main/java/com/AmodIT/WelcomeDemo.java
+++ src/main/java/com/AmodIT/WelcomeDemo.java
@@ -4,6 +4,7 @@ public class WelcomeDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to STS !!!");
+                System.out.println("My first modification");
 
 	}
 
